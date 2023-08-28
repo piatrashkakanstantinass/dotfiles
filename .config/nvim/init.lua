92d52ac9ee040d9ea6835df5b4c42cc2a10591ae
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-08-28 19:11:27 +0300

    add nvim formatting capabilities

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,10 @@
 vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -14,18 +20,19 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  { "nvim-lua/plenary.nvim", lazy = true },
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
     opts = {
       integrations = {
-	mason = true
-      }
-    }
+        mason = true,
+      },
+    },
   },
   {
-    'windwp/nvim-autopairs',
+    "windwp/nvim-autopairs",
     event = "InsertEnter",
     opts = {},
   },
@@ -33,7 +40,6 @@ require("lazy").setup({
   -- LSP
   {
     "hrsh7th/nvim-cmp",
-    version = false,
     event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
@@ -44,57 +50,141 @@ require("lazy").setup({
       local cmp = require("cmp")
       local luasnip = require("luasnip")
       return {
-	snippet = {
-	  expand = function(args)
-	    luasnip.lsp_expand(args.body)
-	  end
-	},
-	sources = cmp.config.sources({
-	  { name = "nvim_lsp" },
-	  { naem = "luasnip" },
-	}),
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }),
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-y>"] = cmp.mapping.confirm(),
+        }),
       }
     end,
   },
   "hrsh7th/cmp-nvim-lsp",
   {
     "williamboman/mason.nvim",
-    opts = {}
+    opts = {},
   },
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = {
       "nvim-lspconfig",
-      "hrsh7th/cmp-nvim-lsp"
+      "hrsh7th/cmp-nvim-lsp",
     },
     opts = {
+      automatic_installation = true,
       handlers = {
-	function (server_name)
-	  local capabilities = require("cmp_nvim_lsp").default_capabilities()
-	  require("lspconfig")[server_name].setup({
-	    capabilities = capabilities
-	  })
-	end
-      }
-    }
+        function(server_name)
+          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+          require("lspconfig")[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+        ["lua_ls"] = function()
+          require("lspconfig")["lua_ls"].setup({
+            settings = {
+              Lua = {
+                workspace = {
+                  checkThirdParty = false,
+                },
+                format = {
+                  enable = false,
+                },
+              },
+            },
+          })
+        end,
+      },
+    },
   },
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      { "folke/neodev.nvim", opts = {} },
+    },
+    keys = {
+      {
+        "<leader>cf",
+        vim.lsp.buf.format,
+        mode = "n",
+      },
+    },
   },
   {
     "L3MON4D3/LuaSnip",
     build = "make install_jsregexp",
     opts = {
-
-    }
+      delete_check_events = "TextChanged",
+    },
+    keys = {
+      {
+        "<tab>",
+        function()
+          require("luasnip").jump(1)
+        end,
+        mode = "s",
+      },
+      {
+        "<s-tab>",
+        function()
+          require("luasnip").jump(-1)
+        end,
+        mode = { "i", "s" },
+      },
+      {
+        "<tab>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+    },
   },
   "saadparwaiz1/cmp_luasnip",
-
+  {
+    "folke/neodev.nvim",
+    opts = {},
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "mason.nvim" },
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+        sources = {
+          nls.builtins.formatting.stylua,
+        },
+      }
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "jose-elias-alvarez/null-ls.nvim",
+    },
+    opts = {
+      handlers = {
+        function() end,
+      },
+    },
+  },
 }, {
   install = {
-    colorscheme = { "catppuccin" }
-  }
+    colorscheme = { "catppuccin" },
+  },
 })
-
 vim.cmd("colorscheme catppuccin")
