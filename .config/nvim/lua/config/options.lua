ccb23a155b3ba64a5bbb01b768e6cb9527da1f26
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-09-12 12:31:11 +0300

    nvim: use system clipboard

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,4 +5,5 @@ vim.opt.softtabstop = 2
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true
-vim.opt.showmode = false;
+vim.opt.showmode = false
+vim.opt.clipboard = "unnamedplus"
